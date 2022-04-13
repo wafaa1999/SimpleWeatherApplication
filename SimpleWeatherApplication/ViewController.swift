d894f3eb13e8b35fc9b14ed210b545cf9754cedf
Wafaa <w.daas.1999@hotmail.com>
2022-04-13 22:36:28 +0300

    Initial Commit

---
+++ SimpleWeatherApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApplication
+//
+//  Created by Wafaa Dwikat on 13/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
